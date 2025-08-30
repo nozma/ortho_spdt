7970282574a63e0352e63614d111f0c61f0507fe
nozma <co.panda.fc3s@gmail.com>
2025-08-30 17:31:19 +0900

    arrange layer0

--- qmk_firmware/keyboards/split_ortho4x6/keymaps/vial/keymap.c
+++ qmk_firmware/keyboards/split_ortho4x6/keymaps/vial/keymap.c
@@ -12,17 +12,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-                   KC_Q,            KC_W,            KC_E,            KC_R,
-                   KC_T,         QK_BOOT,      LT(3,KC_A),            KC_S,
-                   KC_D,            KC_F,            KC_G,  KC_TRANSPARENT,
-           LSFT_T(KC_Z),            KC_X,            KC_C,            KC_V,
-                   KC_B,     KC_LEFT_GUI,     KC_LEFT_ALT, LT(1,KC_LANGUAGE_2),
-         LT(5,KC_ENTER),         QK_BOOT,            KC_Y,            KC_U,
-                   KC_I,            KC_O,            KC_P,  KC_TRANSPARENT,
-                   KC_H,            KC_J,            KC_K,      LT(4,KC_L),
-           KC_SEMICOLON,            KC_N,            KC_M,        KC_COMMA,
-                 KC_DOT, RSFT_T(KC_SLASH),  LT(6,KC_SPACE), LT(2,KC_LANGUAGE_1),
-              KC_ESCAPE
+                /* 左手側 */
+                   KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                QK_BOOT,      
+                   LT(3,KC_A),      KC_S,            KC_D,            KC_F,            KC_G,                KC_NO,
+                   LSFT_T(KC_Z),    KC_X,            KC_C,            KC_V,            KC_B,     
+                   KC_LEFT_GUI,                                       KC_LEFT_ALT,     LT(1,KC_LANGUAGE_2), LT(5,KC_ENTER),        
+                /* 右手側 */
+                QK_BOOT,            
+                   KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,  
+                KC_NO,
+                   KC_H,            KC_J,            KC_K,            LT(4,KC_L),      KC_SEMICOLON,            
+                   KC_N,            KC_M,            KC_COMMA,        KC_DOT,          RSFT_T(KC_SLASH),  
+                LT(6,KC_SPACE),     LT(2,KC_LANGUAGE_1),                               KC_ESCAPE
     ),
     [1] = LAYOUT(
               KC_ESCAPE,  (QK_LGUI|KC_W),  (QK_LGUI|KC_E),  (QK_LGUI|KC_R),
